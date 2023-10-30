65c4cdabeb7c0dbdf7caa41fd0358edf7c0c9ed6
Nallib <nallib@termatrac.com>
2023-10-30 16:51:55 +1000

    version 3 operational

--- pages/about.tsx
+++ pages/about.tsx
@@ -22,32 +22,42 @@ export default function Home() {
           <div className="row section-separator">
             <div className="col-sm-12 col-md-6">
               <div className="mh-about-img shadow-2 wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.4s">
-                <img src="assets/images/ab-img.png" alt="" className="img-fluid"/>
+                <img src="assets/images/ab-img.png" alt="" className="img-fluid" />
               </div>
             </div>
             <div className="col-sm-12 col-md-6">
               <div className="mh-about-inner">
                 <h2 className="wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.1s">About Me</h2>
-                <p className="wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.2s">Hello, I’m a Patrick,
-                  web-developer based on Paris.
-                  I have rich experience in web site design & building
-                  and customization. Also I am good at</p>
+                <p className="wow fadeInUp" data-wow-duration="0.8s"
+                  data-wow-delay="0.2s">Hello, I’m a Nallib,
+                  web-developer based on Australia.
+                  I have rich experience in multiple industries and technologies.</p>
                 <div className="mh-about-tag wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.3s">
                   <ul>
                     <li><img alt="PHP logo" src="assets/images/php-64.png " /></li>
                     <li><img alt="Linux logo" src="assets/images/linux-64.png " /></li>
                     <li><img alt="Unity logo" src="assets/images/unity-64.png " /></li>
-                    {/* <li><span>php</span></li> */}
-                    <li><span>html</span></li>
-                    <li><span>css</span></li>
-                    <li><span>php</span></li>
-                    <li><span>wordpress</span></li>
-                    <li><span>React</span></li>
-                    <li><span>Javascript</span></li>
+                    <li><img alt="CSS logo" src="assets/images/css-64.png " /></li>
+                    <li><img alt="React logo" src="assets/images/react-64.png " /></li>
+                    <li><img alt="Wordpress logo"
+                      src="assets/images/wordpress-64.png " /></li>
+                    <li><img alt="Adobe P logo"
+                      src="assets/images/adobe-ps-64.png " /></li>
+                    <li><img alt="Google Cloud logo"
+                      src="assets/images/google-cloud-64.png " /></li>
+                    <li><img alt="AWS logo"
+                      src="assets/images/amazon-aws-64.png " /></li>
+
                   </ul>
                 </div>
-                <a href="#" className="btn btn-fill wow fadeInUp" data-wow-duration="0.8s"
-                  data-wow-delay="0.4s">Downlaod CV <i className="fa fa-download"></i></a>
+                <a
+                  target="_new"
+                  href="https://ntalam.com/contact-ntalam/"
+                  className="btn btn-fill wow fadeInUp"
+                  data-wow-duration="0.8s"
+                  data-wow-delay="0.4s">Contact me
+                  <i className="fa fa-address-book" />
+                </a>
               </div>
             </div>
           </div>
